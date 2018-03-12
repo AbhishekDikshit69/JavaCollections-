e557c18c706c7add5a189adfa88c47d31f041e85
AbhishekDikshit69 <abhishekdikshit@outlook.com>
2018-03-12 10:49:57 +0530

    added treemap

---
+++ treemapdemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class treemapdemo{
+
+	public static void main(String args[])
+	{
+
+		TreeMap  m=new TreeMap();
+
+		m.put(107,"Abhishek");
+		m.put(101,"Sadhana");
+		m.put(104,"Bhawani Shankar");
+
+		System.out.println(m);
+
+	}
+}
